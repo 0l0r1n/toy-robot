5d75fd35bf959906a5b50571f7fbe23aa4894baa
Leo Alves <leonardo.filipe.alves@gmail.com>
2019-08-09 18:30:03 +0200

    chore: first commit

---
+++ src/main/java/com/rea_group/toyrobot/ToyRobotApplication.java
@@ -0,0 +1,13 @@
+package com.rea_group.toyrobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyRobotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyRobotApplication.class, args);
+	}
+
+}
