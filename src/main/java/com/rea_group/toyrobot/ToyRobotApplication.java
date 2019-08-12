bca5c72b194a00b5aa7d8b1a4ab2faed688bc620
Leo Alves <leonardo.filipe.alves@gmail.com>
2019-08-12 09:23:24 +0200

    chore: remove unnecessary \s

--- src/main/java/com/rea_group/toyrobot/ToyRobotApplication.java
+++ src/main/java/com/rea_group/toyrobot/ToyRobotApplication.java
@@ -22,8 +22,8 @@ public class ToyRobotApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) {
 		String newLine = System.getProperty("line.separator");
-		System.out.println("Starting application - welcome to Toy Robot " + newLine +
-				" Toy Robot is very simple, you have the following commands:" + newLine +
+		System.out.println("Starting application - welcome to Toy Robot!" + newLine +
+				"Toy Robot is very simple, you have the following commands:" + newLine +
 				"MOVE: robot will go forward" + newLine +
 				"LEFT: robot turns left" + newLine +
 				"RIGHT: robot turns right" + newLine +
